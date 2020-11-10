be09cfa578473c147709b2d25274695eafaff9c1
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-11-11 01:35:45 +0300

    add data on the screen

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="home">
     <div class="results">
-      <Card />
+      <p>{{ shows }}</p>
+      <p>----------------</p>
+      <p>{{ genres }}</p>
+      <p>----------------</p>
+      <p>{{ showInfo }}</p>
+      <p>----------------</p>
+      <p>{{ showImages }}</p>
+      <p>----------------</p>
+      <p>{{ showCast }}</p>
+      <p>----------------</p>
+      <p>{{ showEpisodes }}</p>
+      <p>----------------</p>
+      <p>{{ showSeasons }}</p>
+      <p>----------------</p>
+      <p>{{ showCrew }}</p>
+      <p>----------------</p>
+
+      <Card :name="shows.name" :rating="shows.rating.average" :year="shows.premiered"/>
       <Card />
       <Card />
       <Card />
@@ -20,6 +37,41 @@ export default {
   name: 'Home',
   components: {
     Card
+  },
+  computed: {
+    shows() {
+      return this.$store.state.showInfo
+    },
+    genres() {
+      return this.$store.state.genres
+    },
+    showInfo() {
+      return this.$store.state.showInfo
+    },
+    showImages() {
+      return this.$store.state.showImages
+    },
+    showCast() {
+      return this.$store.state.showCast
+    },
+    showEpisodes() {
+      return this.$store.state.showEpisodes
+    },
+    showSeasons() {
+      return this.$store.state.showSeasons
+    },
+    showCrew() {
+      return this.$store.state.showCrew
+    }
+  },
+  mounted() {
+    this.$store.dispatch('fetchAllShows')
+    this.$store.dispatch('fetchShow')
+    this.$store.dispatch('fetchShowImages')
+    this.$store.dispatch('fetchShowCast')
+    this.$store.dispatch('fetchShowEpisodes')
+    this.$store.dispatch('fetchShowSeasons')
+    this.$store.dispatch('fetchShowCrew')
   }
 }
 </script>
