1cdf190c20c6c6d54cf7ff7b7d2352601451627c
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-11-10 19:47:54 +0300

    configure card and style

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,16 @@
 <template>
   <Container>
     <div class="card">
-      <h1>Card</h1>
+      <div class="card-image">
+        <img src="../assets/images/StarWars.jpg" alt="">
+      </div>
+      <div class="card-infos">
+        <h2>Title: </h2>
+        <p>Year: </p>
+        <p>Seasons: </p>
+        <p>Rating Score: </p>
+        <p>+ Follow</p>
+      </div>
     </div>
   </Container>
 </template>
@@ -18,15 +27,25 @@ export default {
 
 <style scoped>
 .card{
-  width: 30%;
-  max-width: 34%;
+  width: 250px;
   height: 300px;
+  padding: 20px;
   border: 1px solid black;
   border-radius: 4px;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  &:hover{
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-  }
+  transition: transform 100ms;
+  margin-bottom: 24px;
+}
+.card-image > img{
+  width: 250px;
+  height: 150px;
+  object-fit: contain;
+}
+.card:hover{
+  transform: scale(1.09);
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+.card-infos > h2{
+  padding-top: 10px;
 }
 </style>
