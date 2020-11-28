e2db01e40f6b1a1fd1687ebc2427c242976ce2bf
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-11-28 23:30:41 +0300

    add sorting

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="home">
+    <div class="sorting">
+      <button @click="sortByNameAsc('name')" v-if="SHOW_LIST.length > 0">
+        <A2ZIcon />
+      </button>
+      <button @click="sortByNameDesc('name')" v-if="SHOW_LIST.length > 0">
+        <Z2AIcon />
+      </button>
+    </div>
     <div class="results">
       <Card
         v-for="show in showSearch"
@@ -22,11 +30,15 @@
 import Card from '@/components/Card'
 import { Jumper } from 'vue-loading-spinner'
 import { mapGetters } from 'vuex'
+import A2ZIcon from '@/icons/A2ZIcon.svg'
+import Z2AIcon from '@/icons/Z2AIcon.svg'
 export default {
   name: 'Home',
   components: {
     Card,
-    Jumper
+    Jumper,
+    A2ZIcon,
+    Z2AIcon
   },
   computed: {
     ...mapGetters(['SHOW_LIST', 'GET_INPUT']),
@@ -38,6 +50,14 @@ export default {
   },
   mounted() {
     this.$store.dispatch('fetchAllShows')
+  },
+  methods: {
+    sortByNameAsc(prop) {
+      this.SHOW_LIST.sort((a, b) => (a[prop] < b[prop] ? -1 : 1))
+    },
+    sortByNameDesc(prop) {
+      this.SHOW_LIST.sort((a, b) => (a[prop] < b[prop] ? 1 : -1))
+    }
   }
 }
 </script>
@@ -53,5 +73,24 @@ export default {
 }
 /deep/.loading div {
   background-color: rgb(var(--yellow)) !important;
+}
+.sorting {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-bottom: 16px;
+}
+.sorting button {
+  width: 36px;
+  height: 36px;
+  margin-right: 8px;
+  box-shadow: var(--box-shadow);
+  background-color: #2c2c2c;
+}
+.sorting button:focus {
+  box-shadow: none;
+}
+.sorting button svg {
+  fill: white;
 }
 </style>
